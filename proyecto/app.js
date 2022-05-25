8e8d433a642e36f3dc18a56745bb68c79039e06e
roccofodaro <rokitofodaro@gmail.com>
2022-05-25 20:24:49 -0300

    Paginas enlazadas al home

--- proyecto/app.js
+++ proyecto/app.js
@@ -13,4 +13,15 @@ app.get('/detalle', function (req, res){
   res.sendFile(path.join(__dirname, '/views/detalle.html'))
 });
 
+app.get('/carrito', function (req, res){
+  res.sendFile(path.join(__dirname, '/views/carrito.html'))
+});
+app.get('/login', function (req, res){
+  res.sendFile(path.join(__dirname, '/views/login.html'))
+});
+
+app.get('/register', function (req, res){
+  res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
 app.listen(PORT, function() {console.log(`Servidor corriendo en el puerto ${PORT}`)});
