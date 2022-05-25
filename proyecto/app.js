3a53320edeffa3fe7f62bdf698c63d43ea25ba23
Diegos01 <diegosalvatore@gmail.com>
2022-05-25 19:39:42 -0300

    Carrito corregido

--- proyecto/app.js
+++ proyecto/app.js
@@ -5,12 +5,23 @@ const path = require("path");
 
 app.use(express.static("public"));
 
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 
-app.get('/detalle', function (req, res){
+app.get('/detalle', function (req, res) {
   res.sendFile(path.join(__dirname, '/views/detalle.html'))
 });
 
-app.listen(PORT, function() {console.log(`Servidor corriendo en el puerto ${PORT}`)});
+app.get('/carrito', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/carrito.html'))
+});
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/login.html'))
+});
+app.get('/register', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
+
+app.listen(PORT, function () { console.log(`Servidor corriendo en el puerto ${PORT}`) });
