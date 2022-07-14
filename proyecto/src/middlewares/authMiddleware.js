402170c7e44068c4af7b8975be71bef3ef22e8c0
roccofodaro <rokitofodaro@gmail.com>
2022-07-13 23:30:20 -0300

    Solucion carpetas clonadas

--- proyecto/src/middlewares/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-function authMiddleware(req, res, next) {
-
-    if (!req.session.userLoggen) {
-        return res.redirect('/login')
-    }
-    next();
-
-}
-module.exports = authMiddleware;
--- proyecto/src/middlewares/guestMiddleware.js
+++
@@ -1,9 +0,0 @@
-function guestMiddleware(req, res, next) {
-
-    if (req.session.userLoggen) {
-        return res.redirect('/users/userView')
-    }
-    next();
-
-}
-module.exports = guestMiddleware;
