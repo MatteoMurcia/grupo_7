0cb3573c13f9dfcdc4c19d7a2f23b3272988608f
roccofodaro <rokitofodaro@gmail.com>
2022-09-18 23:15:51 -0300

    Modified port

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,9 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
     "database": "mizu",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": "8889"
   },
   "test": {
     "username": "root",
